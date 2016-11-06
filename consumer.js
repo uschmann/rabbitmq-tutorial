5d74b96036f8f1239c22ae2b7914ec5950a2a5a1
Andre Uschmann <mail@andre-uschmann.de>
2016-11-06 14:43:22 +0100

    Implemented 2nd tutorial

--- consumer.js
+++ consumer.js
@@ -2,13 +2,18 @@ var amqp = require('amqplib/callback_api');
 
 amqp.connect(`amqp://${process.env.RABBIT_HOST || 'localhost'}`, function(err, conn) {
   conn.createChannel(function(err, ch) {
-    var q = 'hello';
+    var q = 'tasks';
 
-    ch.assertQueue(q, {durable: false});
+    ch.assertQueue(q, {durable: true});
 
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
     ch.consume(q, function(msg) {
+      var secs = msg.content.toString().split('.').length - 1;
+
       console.log(" [x] Received %s", msg.content.toString());
-    }, {noAck: true});
+      setTimeout(function() {
+        ch.ack(msg);
+        console.log(" [x] Done");
+      }, secs * 1000);
+    }, {noAck: false});
   });
 });
--- producer.js
+++ producer.js
@@ -1,17 +1,18 @@
 var amqp = require('amqplib/callback_api');
 
+var msg = process.argv.slice(2).join(' ') || "Hello World!";
+
 amqp.connect(`amqp://${process.env.RABBIT_HOST || 'localhost'}`, function(err, conn) {
   if(err) {
     console.log('Error', err);
   }
   else {
     conn.createChannel(function(err, ch) {
-      var q = 'hello';
+      var q = 'tasks';
 
-      ch.assertQueue(q, {durable: false});
-      // Note: on Node 6 Buffer.from(msg) should be used
-      ch.sendToQueue(q, Buffer.from('Hello World!'));
-      console.log(" [x] Sent 'Hello World!'");
+      ch.assertQueue(q, {durable: true});
+      ch.sendToQueue(q, new Buffer(msg), {persistent: true});
+      console.log(" [x] Sent '%s'", msg);
 
       setTimeout(function() { conn.close(); process.exit(0) }, 500);
     });
